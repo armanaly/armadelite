4356439ea88523cef69f14a633c53b686b48151c
Anthony <anthony.dupont@eduwell.cz>
2017-05-17 23:26:19 +0200

    task_15v1
    Former-commit-id: a29a1b0c54c9bd4fd6d391281d9fadf5e83a4214

--- assets/app/global.ts
+++ assets/app/global.ts
@@ -1,6 +1,6 @@
 
 export const GlobalVariable = Object.freeze({
-    //BASE_URL : 'https://apiflask.herokuapp.com/',
-    BASE_URL: 'http://localhost:5000/',
+    BASE_URL : 'https://apiflask.herokuapp.com/',
+    //BASE_URL: 'http://localhost:5000/',
     APP_NAME: 'ballet'
 });
