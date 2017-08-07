60122bb5ab45b374991728c000fe3e7aaa5dee8d
Anthony <anthony.dupont@eduwell.cz>
2017-08-07 11:13:20 +0200

    rm unused files
    Former-commit-id: d82f39e0bad0ac9b41e7b0488d2597d22fd028c7

--- app.js
+++ app.js
@@ -83,12 +83,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');
-//   next();
-// });
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');
+  next();
+});
 
 // app.use('/marque', marqueRoutes);
 // app.use('/vehicule', vehiculeRoutes);
